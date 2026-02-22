cad26b9cfbbda0e5d7ed5f3f1ed643f415f88cdd
Nandanivakodikar20 <vakodikarnandani@gmail.com>
2026-02-22 17:08:01 +0530

    Updated full_pyramid pattern

--- full_pyramid.py
+++ full_pyramid.py
@@ -2,18 +2,6 @@
 
 rows = 5
 
-for i in range(rows, 0, -1):
-    for j in range(rows - i):
-        print(" ", end="")
-    for k in range(i):
-        print("* ", end="")
-    print()
-    
-
-
-
-rows = 5
-
 for i in range(1, rows + 1):
     # Print leading spaces
     for space in range(rows - i):
@@ -24,3 +12,16 @@ for i in range(1, rows + 1):
         print(chr(65 + j), end=" ")
 
     print()
+    
+    
+rows = 5
+
+for i in range(rows, 0, -1):
+    for j in range(rows - i):
+        print(" ", end="")
+    for k in range(i):
+        print("* ", end="")
+    print()
+    
+
+
